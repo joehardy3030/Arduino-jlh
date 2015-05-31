6a52e4640aa877ec3042861c596b488fa0c50f10
joehardy3030 <jlhardyphd@gmail.com>
2015-05-30 17:00:23 -0700

    Using NSURLSession and displaying HTTP data string in app

--- Smart House/Smart House/ViewController.swift
+++ Smart House/Smart House/ViewController.swift
@@ -16,51 +16,87 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     //open the relevant url in Safari for turning on the heater
     @IBAction func heaterOn() {
-        if let url = NSURL(string: "http://10.0.0.12/arduino/on3") {
-            UIApplication.sharedApplication().openURL(url)
-        }
+        let url = NSURL(string: "http://10.0.0.12/arduino/on3")
+        var dataString:String = "No connection"
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            dataString = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                // Update the UI on the main thread.
+                self.textLabel.text = dataString
+            } //dispatch_async
+        } //NSURLSession
+        task.resume()
     }
     
     //open the relevant url in Safari for turning off the heater
     @IBAction func heaterOff() {
-        if let url = NSURL(string: "http://10.0.0.12/arduino/off") {
-            UIApplication.sharedApplication().openURL(url)
-        }
+        let url = NSURL(string: "http://10.0.0.12/arduino/off")
+        var dataString:String = "No connection"
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            dataString = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                // Update the UI on the main thread.
+                self.textLabel.text = dataString
+            } //dispatch_async
+        } //NSURLSession
+        task.resume()
     }
 
     //open the relevant url in Safari for closing the loop on garage door opener
     @IBAction func garageDoor() {
-        if let url = NSURL(string: "http://10.0.0.20/arduino/press") {
-            UIApplication.sharedApplication().openURL(url)
-        }
-    }
-
-    //open the relevant url in Safari for turning off the outlet for the stereo
-    @IBAction func outletOff() {
-        if let url = NSURL(string: "http://10.0.0.20/arduino/outletOff") {
-            UIApplication.sharedApplication().openURL(url)
-        }
-    }
-
-    
-    // Open a NUSRLSession to flip the switch and suck in the data
-    // https://www.youtube.com/watch?v=xt-Q4c1kMBQ
-    @IBAction func outletOn() {
-        
-        let url = NSURL(string: "http://10.0.0.20/arduino/outletOn")
-        
+        let url = NSURL(string: "http://10.0.0.20/arduino/press")
+        var dataString:String = ""
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
-        }
-        
+            dataString = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                // Update the UI on the main thread.
+                self.textLabel.text = dataString
+            } //dispatch_async
+        } //NSURLSession
         task.resume()
-        //if let url = NSURL(string: "http://10.0.0.20/arduino/outletOn") {
-        //    UIApplication.sharedApplication().openURL(url)
-       // }
+        
+//        if let url = NSURL(string: "http://10.0.0.20/arduino/press") {
+//            UIApplication.sharedApplication().openURL(url)
+//        }
     }
 
+    @IBAction func outletOn(sender: AnyObject) {
+        let url = NSURL(string: "http://10.0.0.20/arduino/outletOn")
+        var dataString:String = ""
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            dataString = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                // Update the UI on the main thread.
+                self.textLabel.text = dataString
+            } //dispatch_async
+        } //NSURLSession
+        task.resume()
+    } //outletOn
     
-}
+
+    // https://www.youtube.com/watch?v=xt-Q4c1kMBQ
+    // Open a NUSRLSession to flip the switch and suck in the data
+    @IBAction func outletOff() {
+        let url = NSURL(string: "http://10.0.0.20/arduino/outletOff")
+        var dataString:String = ""
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            dataString = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+        
+            dispatch_async(dispatch_get_main_queue()) {
+                // Update the UI on the main thread.
+                self.textLabel.text = dataString
+             } //dispatch_async
+        } //NSURLSession
+        task.resume()
+     } //outletOff()
+
+} //UI controller
 
