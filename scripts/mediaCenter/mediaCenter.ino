4920aeb8fe8965febe35b905768fdef02ff4c561
joehardy3030 <jlhardyphd@gmail.com>
2015-06-17 23:08:38 -0700

    ripped Temboo from mediaCenter

--- scripts/mediaCenter/mediaCenter.ino
+++ scripts/mediaCenter/mediaCenter.ino
@@ -1,15 +1,11 @@
 // This program is designed to control a relay to turn a heater on and off
 // joe$ ssh root@10.0.0.20 'telnet localhost 6571'
 
-#include <Temboo.h>
 #include <Bridge.h>
-#include <Console.h>
 #include <YunServer.h>
 #include <YunClient.h>
 #include <Process.h>
 #include <IRremote.h>
-
-#include "TembooAccount.h"
 #include "DHT.h"
 
 //The panasonic protocol is 48-bits long, all the commands has the same address (or pre data) and then followed by a 32 bit code
@@ -29,11 +25,6 @@ IRsend irsend;
 // DHT instance
 DHT dht(DHTPIN, DHTTYPE);
                                          
-// Your Google Docs data
-const String GOOGLE_USERNAME = "jlhardyphd@gmail.com";
-const String GOOGLE_PASSWORD = "azjfsgddtpzuawcv";
-const String SPREADSHEET_TITLE = "heaterTimer";
-
 const int buttonDown = 0;
 const int buttonUp = 1;
 const int relayOn = 0;
@@ -55,14 +46,8 @@ void setup() {
   YunClient client;
   Serial.begin(115200);
   Bridge.begin();
-  Console.begin();
   server.listenOnLocalhost();
   server.begin();
-  if (!date.running())  {
-    date.begin("date");
-    date.addParameter("+%D-%T");
-    date.run();
-  }
   delay(1000);
   intializeRelay(client, doorRelayPin); //set the relay pin to OUTPUT and turn it off
   intializeRelay(client, outletRelayPin); //set the relay pin to OUTPUT and turn it off
@@ -92,9 +77,6 @@ void process(YunClient client) {
   }
   if (command == "outletOff") {
     outletOffCommand(client);
-  }
-  if (command == "append") {
-    appendCommand(client);
   }
   if (command == "TVon") {
     tvOnCommand(client);
@@ -190,87 +172,5 @@ void outletOffCommand(YunClient client) {
   client.println(" min");
   writePin(client, outletRelayPin, relayOff);
   client.stop();
-}
-
-void appendCommand(YunClient client) {
-  client.println(F("append"));
-  datePrint(client); // Print date
-  tempPrint(client); // Temp date
-  client.stop();
-  while(client.connected());
-  runAppendRow();
-}
-
-
-// Function to add data to Google Docs
-void runAppendRow() {
-  TembooChoreo AppendRowChoreo; 
-
-  // Invoke the Temboo client
-  AppendRowChoreo.begin();
-
-  // Set Temboo account credentials
-  AppendRowChoreo.setAccountName(TEMBOO_ACCOUNT);
-  AppendRowChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
-  AppendRowChoreo.setAppKey(TEMBOO_APP_KEY);
-  
-  // Identify the Choreo to run
-  AppendRowChoreo.setChoreo("/Library/Google/Spreadsheets/AppendRow");
-
-  // your Google username (usually your email address)
-  AppendRowChoreo.addInput("Username", GOOGLE_USERNAME);
-
-  // your Google account password
-  AppendRowChoreo.addInput("Password", GOOGLE_PASSWORD);
-  
-  // the title of the spreadsheet you want to append to
-  AppendRowChoreo.addInput("SpreadsheetTitle", SPREADSHEET_TITLE);
-  
-  // Restart the date process:
-  if (!date.running())  {
-    date.begin("date");
-    date.addParameter("+%D-%T");
-    date.run();
-   }
-  
-  // Get date
-  String timeString = date.readString(); 
-  humidity = dht.readHumidity();
-  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
-  
-  String heater = "off";
-  if (digitalRead(doorRelayPin) == buttonDown) {
-    heater = "on";
-  }
-  
-  // Format data
-  String data = "";
-  
-  data = data + timeString + "," + temperature + "," + humidity + "," + heater;
-  
-  // Set Choreo inputs
-  AppendRowChoreo.addInput("RowData", data);
-
-  // Run the Choreo
-  unsigned int returnCode = AppendRowChoreo.run();
-
-  // A return code of zero means everything worked
-  if (returnCode == 0) {
-      Serial.println("Completed execution of the /Library/Google/Spreadsheets/AppendRow Choreo.\n");
-      Console.println("Completed execution of the /Library/Google/Spreadsheets/AppendRow Choreo.\n");
-    }
-  else {
-    // A non-zero return code means there was an error
-    // Read and print the error message
-    while (AppendRowChoreo.available()) {
-      char c = AppendRowChoreo.read();
-      Serial.print(c); 
-      Console.print(c); 
-    }
-    Serial.println(); 
-    Console.println();   
-  }
-  
-  AppendRowChoreo.close(); 
 }
 
