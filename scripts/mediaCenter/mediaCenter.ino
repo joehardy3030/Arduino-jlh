d06951385b8714a55ed9c2a7d89b141bfdcc5e3d
Joseph Hardy <joe@Josephs-MacBook-Pro.local>
2015-06-17 22:56:31 -0700

    vol up/down

--- scripts/mediaCenter/mediaCenter.ino
+++ scripts/mediaCenter/mediaCenter.ino
@@ -7,9 +7,20 @@
 #include <YunServer.h>
 #include <YunClient.h>
 #include <Process.h>
+#include <IRremote.h>
 
 #include "TembooAccount.h"
 #include "DHT.h"
+
+//The panasonic protocol is 48-bits long, all the commands has the same address (or pre data) and then followed by a 32 bit code
+#define PanasonicAddress      0x4004     // Panasonic address (Pre data) 
+#define PanasonicPower        0x100BCBD  // Panasonic Power button
+#define PanasonicYellow       0x1004E4F  // Yellow
+#define PanasonicBlue         0x100CECF  // Blue button
+#define PVMinus               0x1008485  // Volume down
+#define PVPlus                0x1000405  // Volume up
+
+IRsend irsend;
 
 // DHT11 sensor pins
 #define DHTPIN A0 
@@ -28,7 +39,7 @@ const int buttonUp = 1;
 const int relayOn = 0;
 const int relayOff = 1;
 const int outletRelayPin = 12;
-const int doorRelayPin = 13;
+const int doorRelayPin = 8;
 
 float humidity;
 float temperature;
@@ -76,7 +87,7 @@ void process(YunClient client) {
     pressCommand(client);
   }
   if (command == "outletOn") {
-    duration = one_hour * 5;
+    duration = one_hour * 4;
     outletOnCommand(client);
   }
   if (command == "outletOff") {
@@ -85,19 +96,19 @@ void process(YunClient client) {
   if (command == "append") {
     appendCommand(client);
   }
+  if (command == "TVon") {
+    tvOnCommand(client);
+  }
+  if (command == "TVvolUp") {
+    tvVolumeUp(client);
+  }
+  if (command == "TVvolDown") {
+    tvVolumeDown(client);
+  }
 }
 
 void writePin(YunClient client, int pin, int value) {
   digitalWrite(pin, value);
-
-  client.print(F("Pin D"));
-  client.print(pin);
-  client.print(F(" set to "));
-  client.println(value);
-
-  String key = "D";
-  key += pin;
-  Bridge.put(key, String(value));
 }
 
 //initialize the relay pin to output and turn it off
@@ -106,49 +117,63 @@ void intializeRelay(YunClient client, int pin) {
   digitalWrite(pin, buttonUp); //turn it off
 }
 
-void pressCommand(YunClient client) {
-  
-  writePin(client, doorRelayPin, buttonDown);
-  delay(1000);
-  writePin(client, doorRelayPin, buttonUp);
-  
-  // Restart the date process:  
+void datePrint(YunClient client) {
   if (!date.running())  {
     date.begin("date");
     date.addParameter("+%D-%T");
     date.run();
-   }
-  // Get date
-  client.println(F("pressed"));
-  humidity = dht.readHumidity();
-  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
+  }
   String timeString = date.readString(); 
   client.print(timeString);
+}  
+
+void tempPrint(YunClient client) {
+  humidity = dht.readHumidity();
+  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
   client.print("Temp: ");
   client.print(temperature);
   client.print("; Humidity: ");
   client.println(humidity);
+}  
+
+void tvOnCommand(YunClient client) {
+  client.println(F("TV on/off"));
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
+  irsend.sendPanasonic(PanasonicAddress,PanasonicPower); // This should turn your TV on and off
+  client.stop();
+ }
+
+void tvVolumeUp(YunClient client) {
+  client.println(F("TV vol up"));
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
+  irsend.sendPanasonic(PanasonicAddress,PVPlus); // This should turn your TV on and off
+  client.stop();
+ }
+
+void tvVolumeDown(YunClient client) {
+  client.println(F("TV vol down"));
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
+  irsend.sendPanasonic(PanasonicAddress,PVMinus); // This should turn TV volume down
+  client.stop();
+ }
+ 
+void pressCommand(YunClient client) {
+  writePin(client, doorRelayPin, buttonDown);
+  delay(1000);
+  writePin(client, doorRelayPin, buttonUp);  
+  client.println(F("Garage remote pressed"));
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
   client.stop();
 }
 
 void outletOnCommand(YunClient client) {
-    // Restart the date process:
-  if (!date.running())  {
-    date.begin("date");
-    date.addParameter("+%D-%T");
-    date.run();
-   }
-  // get humidity and temp
-  humidity = dht.readHumidity();
-  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
-  String timeString = date.readString(); 
-  
   client.println(F("Stereo power on"));
-  client.print(timeString);
-  client.print("Temp: ");
-  client.print(temperature);
-  client.print("; Humidity: ");
-  client.println(humidity);
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
   client.print("Duration: ");
   client.println(duration/(60000));
   m = millis();
@@ -158,19 +183,8 @@ void outletOnCommand(YunClient client) {
 
 void outletOffCommand(YunClient client) {
   client.println(F("Stereo power off"));
-  humidity = dht.readHumidity();
-  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
-    if (!date.running())  {
-    date.begin("date");
-    date.addParameter("+%D-%T");
-    date.run();
-   }
-  String timeString = date.readString(); 
-  client.print(timeString);
-  client.print("Temp: ");
-  client.print(temperature);
-  client.print("; Humidity: ");
-  client.println(humidity);
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
   client.print("Duration: ");
   client.print((millis() - m)/(60000));
   client.println(" min");
@@ -180,15 +194,8 @@ void outletOffCommand(YunClient client) {
 
 void appendCommand(YunClient client) {
   client.println(F("append"));
-  humidity = dht.readHumidity();
-  temperature = (dht.readTemperature() *(9.0/5.0)) + 32.0;
-  String timeString = date.readString(); 
-  client.print(timeString);
-  client.print("Temp: ");
-  client.print(temperature);
-  client.print("; Humidity: ");
-  client.println(humidity);
-  client.println(millis() - m);
+  datePrint(client); // Print date
+  tempPrint(client); // Temp date
   client.stop();
   while(client.connected());
   runAppendRow();
